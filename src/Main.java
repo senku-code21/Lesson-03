9dd75fd5aa5d9ac27e5efe6a7dad201a3929a021
krm <krm@krm.local>
2022-11-23 17:50:54 +0600

    refactoring class code

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int a = 20;
-        int b = 30;
+
+        Main obj = new Main();
+        obj.task1();
+        obj.task2();
+        obj.task3();
+        obj.task4();
+        obj.task5();
+        obj.task6();
+        obj.task7();
+        obj.task8();
+        obj.task9();
+        obj.task10();
+    }
+
+
+    public void task1() {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("1. Please input two integer number:");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        // 1.	Сумма двух чисел, введённых пользователем
         System.out.println(a + " + " + b + " = " + (a+b));
+    }
+
+
+    public void task2() {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("2. Please input two integer number:");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        // 2.	Произведение двух чисел, введённых пользователем
         System.out.println(a + " * " + b + " = " + (a*b));
+    }
 
-        int c = 40;
-        System.out.println("(" + a + " + " + b + ") * " + c + " = " + ((a+b)*c));
-        System.out.println(a + " + " + b + " * " + c + " = " + (a+b*c));
 
+    public void task3() {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("3. Please input two integer number:");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        // 3.	Сравнение двух чисел, введённых пользователем
         if (a > b){
             System.out.println("a > b");
         }else if (a == b) {
@@ -19,66 +57,78 @@ public class Main {
         }else{
             System.out.println("a < b");
         }
+    }
 
-        // Practice task 7
+
+    public void task4() {
         Scanner sc = new Scanner(System.in);
-        int a1;
-        int b1;
-        System.out.println("Please input two integer number:");
-        a1 = sc.nextInt();
-        b1 = sc.nextInt();
-        // 1.	Сумма двух чисел, введённых пользователем
-        System.out.println(a1 + " + " + b1 + " = " + (a1+b1));
-
-        // 2.	Произведение двух чисел, введённых пользователем
-        System.out.println(a1 + " * " + b1 + " = " + (a1*b1));
-
-        // 3.	Сравнение двух чисел, введённых пользователем
-        if (a1 > b1){
-            System.out.println("a > b");
-        }else if (a1 == b1) {
-            System.out.println("a = b");
-        }else{
-            System.out.println("a < b");
-        }
-
+        int a;
+        int b;
+        System.out.println("4. Please input two integer number:");
+        a = sc.nextInt();
+        b = sc.nextInt();
         // 4.	Нахождение остатка от деления на число. Значение вводятся пользователем
-        System.out.println(a1 + " % " + b1 + " = " + (a1%b1));
+        System.out.println(a + " % " + b + " = " + (a%b));
+    }
 
+
+    public void task5() {
+        Scanner sc = new Scanner(System.in);
         // 5.	Вывести результат выражения x² + y - z:2
         int x;
         int y;
         int z;
-        System.out.println("Please input three integer number:");
+        System.out.println("5. Please input three integer number:");
         x = sc.nextInt();
         y = sc.nextInt();
         z = sc.nextInt();
         int tmp = x*x + y - z/2;
         System.out.println("x^2 + y - z/2: " + tmp);
+    }
 
+
+    public void task6() {
+        Scanner sc = new Scanner(System.in);
         // 6.	Площадь квадрата. Сторона задаётся пользователем.
         int l;
-        System.out.println("Please input one integer number:");
+        System.out.println("6. Please input one integer number:");
         l = sc.nextInt();
         System.out.println("s = l^2: " + (l*l));
+    }
 
+
+    public void task7() {
+        Scanner sc = new Scanner(System.in);
         // 7.	Площадь круга. Радиус задаётся пользователем.
         int r;
         double s;
-        System.out.println("Please input one integer number:");
+        System.out.println("7. Please input one integer number:");
         r = sc.nextInt();
-        s = 3.14f * r * r;
+        s = Math.PI * r * r;
         System.out.println("s = Math.PI * r^2: " + s);
+    }
 
+
+    public void task8() {
+        Scanner sc = new Scanner(System.in);
         // 8.	Площадь цилиндра.
         int h;
-        System.out.println("Please input two integer number:");
+        int r;
+        double s;
+        System.out.println("8. Please input two integer number:");
         r = sc.nextInt();
         h = sc.nextInt();
         s = 2 * Math.PI * r * h;
         System.out.println("S = 2 * Math.PI * r * h: " + s);
+    }
 
+
+    public void task9() {
+        Scanner sc = new Scanner(System.in);
         // 9.	Решение квадратного уравнения через дискриминант.
+        int a;
+        int b;
+        int c;
         System.out.println("9. Please input tree integer number:");
         a = sc.nextInt();
         b = sc.nextInt();
@@ -94,8 +144,14 @@ public class Main {
             System.out.println("x1 = " + x1);
             System.out.println("x2 = " + x2);
         }
+    }
 
+
+    public void task10() {
+        Scanner sc = new Scanner(System.in);
         // 10.	Вывести результат выражения
+        int x;
+        int y;
         System.out.println("10. Please input two integer number:");
         x = sc.nextInt();
         y = sc.nextInt();
